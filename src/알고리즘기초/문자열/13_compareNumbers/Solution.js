5665ef56a6c1d357e6a06b3be51e4a624e45c11a
박민수 <pinion7rl@gmail.com>
2021-08-03 02:20:42 +0900

    문자열 #13

---
+++ src/알고리즘기초/문자열/13_compareNumbers/Solution.js
@@ -0,0 +1,14 @@
+function compareNumbers(num1, num2) {
+  if (num1 > num2) {
+    return `${num1}은(는) ${num2}보다 큽니다`
+  }
+  else if (num1 < num2) {
+    return `${num1}은(는) ${num2}보다 작습니다`
+  }
+  else if (num1 === num2) {
+    return `두 수는 같습니다`
+  }
+}
+
+
+
