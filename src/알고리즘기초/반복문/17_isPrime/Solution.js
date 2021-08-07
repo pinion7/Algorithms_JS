0c7da8a058446991019041b3b2cf1ec39f1d1e52
박민수 <pinion7rl@gmail.com>
2021-08-08 01:09:40 +0900

    반복문 #17

---
+++ src/알고리즘기초/반복문/17_isPrime/Solution.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (num === 1) return false;
+  if (num === 2) return true;
+
+  for (let i = 2; i < num; ++i) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+
+
+
